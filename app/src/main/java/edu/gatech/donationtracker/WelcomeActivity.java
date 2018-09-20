2ac65f6707e84c8e151359c9ddbc017677e5e6e6
unknown <maiphamt97@gmail.com>
2018-09-20 09:49:46 -0400

    welcome activity

---
+++ app/src/main/java/edu/gatech/donationtracker/WelcomeActivity.java
@@ -0,0 +1,22 @@
+package edu.gatech.donationtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private String app_name = "Donation Tracker";
+    private String sign_up = "Sign up";
+    private String sign_in = "Sign in";
+    private Button button_sign_up;
+    private Button button_sign_in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        button_sign_in = findViewById(R.id.button_signIn);
+        button_sign_up = findViewById(R.id.button_signUp);
+        //ADD LISTENERS
+    }
+}
