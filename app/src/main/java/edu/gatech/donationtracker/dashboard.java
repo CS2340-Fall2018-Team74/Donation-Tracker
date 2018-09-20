337f401259ddaa240e31f3a24494934aeab2e50e
hxue98 <haoyongxue@gmail.com>
2018-09-20 16:37:54 -0400

    buggy version

---
+++ app/src/main/java/edu/gatech/donationtracker/dashboard.java
@@ -0,0 +1,13 @@
+package edu.gatech.donationtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
