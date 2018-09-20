e87524e55cb83c34e00e789a63c487629c6da1de
hxue98 <haoyongxue@gmail.com>
2018-09-20 10:05:23 -0400

    java controller and model created

---
+++ app/src/main/java/edu/gatech/donationtracker/controller/SignupActivity.java
@@ -0,0 +1,4 @@
+package edu.gatech.donationtracker.controller;
+
+public class SignupActivity {
+}
---
+++ app/src/main/java/edu/gatech/donationtracker/model/UserAccount.java
@@ -0,0 +1,9 @@
+package edu.gatech.donationtracker;
+
+public class UserAccount {
+    private String username;
+    private String password;
+    private String email;
+
+
+}
