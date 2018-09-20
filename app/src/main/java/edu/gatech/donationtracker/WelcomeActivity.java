e2957502061f0cd58ce6113330f7cc58aec16069
unknown <maiphamt97@gmail.com>
2018-09-20 10:08:35 -0400

    fixes

--- app/src/main/java/edu/gatech/donationtracker/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/donationtracker/WelcomeActivity.java
@@ -2,6 +2,7 @@ package edu.gatech.donationtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -18,5 +19,19 @@ public class WelcomeActivity extends AppCompatActivity {
         button_sign_in = findViewById(R.id.button_signIn);
         button_sign_up = findViewById(R.id.button_signUp);
         //ADD LISTENERS
+        button_sign_up.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        button_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
