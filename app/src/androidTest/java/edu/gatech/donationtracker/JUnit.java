9c795ea57f16c7b72658cffb882c42b72f29bc50
Uyen Dinh <udinh3@gatech.edu>
2018-11-09 15:52:42 -0500

    testRemoveUser added

--- app/src/androidTest/java/edu/gatech/donationtracker/JUnit.java
+++ app/src/androidTest/java/edu/gatech/donationtracker/JUnit.java
@@ -4,9 +4,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import edu.gatech.donationtracker.model.Admin;
+import edu.gatech.donationtracker.model.Model;
 import edu.gatech.donationtracker.model.User;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -15,11 +15,23 @@ public class JUnit {
 
     public User user;
     public Admin admin;
+    public User userToRemove1;
+    public User userToRemove2;
+    public Model model;
 
     @Before
     public void setup() {
         user = new User();
         admin = new Admin("E","N", "P");
+
+
+        //testRemoveUser setup
+        userToRemove1 = new User("userToRemove1", "userToRemove1", "userToRemove1");
+        userToRemove2 = new User("userToRemove1", "userToRemove1", "userToRemove1");
+        model = Model.getInstance();
+        model.addAccount(userToRemove1);
+        model.addAccount(userToRemove2);
+
     }
 
     @Test
@@ -29,5 +41,12 @@ public class JUnit {
         assertTrue(user.getIsLocked());
         admin.lockAccount(user);
         assertFalse(user.getIsLocked());
+    }
+
+    @Test
+    public void testRemoveUser() {
+        model.removeAccount(userToRemove1);
+        assertFalse(model.getAccounts().contains(userToRemove1));
+        assertTrue(model.getAccounts().contains(userToRemove2));
     }
 }
